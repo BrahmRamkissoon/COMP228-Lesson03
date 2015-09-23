2931b1f2dd9ae91d7d605c6624c364d0827ba15c
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-09-23 17:23:01 -0400

    added driver class to project

---
+++ src/com/ramkissoon/brahm/MethodsAndArrays.java
@@ -0,0 +1,16 @@
+package com.ramkissoon.brahm;
+
+public class MethodsAndArrays {
+
+	public static void main(String[] args) {
+		long myLongVariable = 100000L;		// here L means literal
+		int myIntVariable;
+		
+		myIntVariable = (int) myLongVariable;
+		
+		System.out.println(myLongVariable);		// use only with primitives
+		
+
+	}
+
+}
