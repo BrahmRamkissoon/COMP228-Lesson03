b7100f237676466d7109a76b6bc8a97f551723b5
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-09-23 22:20:00 -0400

    added game class

---
+++ src/com/ramkissoon/brahm/Game.java
@@ -0,0 +1,37 @@
+package com.ramkissoon.brahm;
+
+import java.security.SecureRandom;
+
+public class Game {
+
+	// CONTRUCTOR ++++++++++++++++++++++++++++++
+	public Game(){
+		
+	}
+	
+	// PUBLIC METHODS ++++++++++++++++++++++++++
+	//rollDice Method returns sum of series of dice rolls
+	public static int rollDice(int numDice, int numSides){
+		// Forces minimum of one die
+		if (numDice <1) {
+			numDice = 1;
+		}
+		
+		// forces minimum of 4 sides
+		if(numSides <4){
+			numSides = 4;
+			
+		}
+		
+		System.out.printf("You selected %d with %d sides %n",  numDice, numSides);
+		
+		
+		SecureRandom generateRandom = new SecureRandom();
+		int result = 0;
+		
+		for(int index=0; index < numDice; index++){
+			result += generateRandom.nextInt(numSides) + 1;
+		}		
+		return result;
+	}
+}
--- src/com/ramkissoon/brahm/MethodsAndArrays.java
+++ src/com/ramkissoon/brahm/MethodsAndArrays.java
@@ -1,35 +1,25 @@
 package com.ramkissoon.brahm;
 
 import java.security.SecureRandom;
+import java.util.Scanner;
+
+
 
 public class MethodsAndArrays {
-
-	public static void main(String[] args) {
-		SecureRandom generateRandom = new SecureRandom();
-		int die1 = 0;
-		int die2 = 0;
-		int dice = 0;
-		
-		// generate a number betwween 2 and 12
-		
-		die1 = generateRandom.nextInt(6) + 1;
-		die2 = generateRandom.nextInt(6) + 1;
-		
-		dice = die1 + die2;
-		
-		System.out.println(rollDice(2,6));
-		
-
-	}
 	
-	public static int rollDice(int numDice, int numSides){
-		SecureRandom generateRandom = new SecureRandom();
-		int result = 0;
+	public static void main(String[] args) {
+		Game  game = new Game();
 		
-		for(int index=0; index < numDice-1; index++){
-			result += generateRandom.nextInt(numSides) + 1;
-		}
+		Scanner input = new Scanner(System.in);
+		int numDice = 0;	
+		int numSides = 0;	
 		
-		return result;
+		System.out.println("Enter the number of die");
+		numDice = input.nextInt();
+		
+		System.out.println("Enter the number of sides");
+		numDice = input.nextInt();
+		
+		System.out.printf("You rolled %d%n", game.rollDice(numDice,  numSides));		
 	}
 }
